8af91259910bafe18975b50c43491b042bee76b0
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-07-07 20:40:10 +0300

    fix: add some data to users migration file

--- database/migrations/2023_07_06_184810_create_users_table.php
+++ database/migrations/2023_07_06_184810_create_users_table.php
@@ -18,9 +18,11 @@ return new class extends Migration
             $table->bigInteger('mobile_number');
             $table->foreignId('city_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->string('code');
+            $table->string('image')->default('default.jpg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('roles_name');
             $table->rememberToken();
             $table->timestamps();
         });
