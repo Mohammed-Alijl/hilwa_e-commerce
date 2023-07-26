d8d9d854dd869d8185cba814fb38ae9fffcde477
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-07-26 22:30:35 +0300

    fix: put permission midelware to timeslot controller

--- app/Http/Controllers/TimeslotController.php
+++ app/Http/Controllers/TimeslotController.php
@@ -7,10 +7,16 @@ use App\Http\Requests\Timeslot\EditRequest;
 use App\Http\Requests\Timeslot\IndexRequest;
 use App\Http\Requests\Timeslot\StoreRequest;
 use App\Http\Requests\Timeslot\UpdateRequest;
-use Illuminate\Http\Request;
 
 class TimeslotController extends Controller
 {
+    function __construct()
+    {
+        $this->middleware('permission:view_timeslot', ['only' => ['index','show']]);
+        $this->middleware('permission:add_timeslot', ['only' => ['create','store']]);
+        $this->middleware('permission:edit_timeslot', ['only' => ['edit','update']]);
+        $this->middleware('permission:delete_timeslot', ['only' => ['destroy']]);
+    }
     /**
      * Display a listing of the resource.
      */
